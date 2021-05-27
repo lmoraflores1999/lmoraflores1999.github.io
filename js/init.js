c3bc665521a20f9484e238827cad71abfffc87ad
lmoraflores1999 <84933858+lmoraflores1999@users.noreply.github.com>
2021-05-27 14:20:05 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBvTDx8IZ8anOIftJ2pAPvWg3rnzcl47yU",
+    authDomain: "lmoraflores1999-8de46.firebaseapp.com",
+    projectId: "lmoraflores1999-8de46",
+    storageBucket: "lmoraflores1999-8de46.appspot.com",
+    messagingSenderId: "440737811802",
+    appId: "1:440737811802:web:839e1527dce08c7a003763",
+    measurementId: "G-YQ992M3Z1V"
+  });
